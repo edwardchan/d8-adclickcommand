6ab18782876fbdac2ed7ca7d6a3a72e00f404cbd
Edward Chan <edwardchan1350@yahoo.com>
2017-03-14 10:18:35 -0400

    Initial commit with indentation fix.

--- src/Entity/Controller/AdClickCommandController.php
+++ src/Entity/Controller/AdClickCommandController.php
@@ -13,36 +13,39 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
  */
 class AdClickCommandController extends ControllerBase implements ContainerInjectionInterface {
 
-    /**
-     * The key value expirable factory.
-     *
-     * @var \Drupal\Core\KeyValueStore\KeyValueExpirableFactoryInterface
-     */
-    protected $keyValueExpireFactory;
+  /**
+   * The key value expirable factory.
+   *
+   * @var \Drupal\Core\KeyValueStore\KeyValueExpirableFactoryInterface
+   */
+  protected $keyValueExpireFactory;
 
-    /**
-     * Constructs a AdClickCommandController object.
-     *
-     * @param \Drupal\Core\KeyValueStore\KeyValueExpirableFactoryInterface $key_value_expire_factory
-     *   The key value expirable factory.
-     */
-    public function __construct(KeyValueExpirableFactoryInterface $key_value_expire_factory, EntityTypeManagerInterface $entity_type_manager) {
-        $this->keyValueExpireFactory = $key_value_expire_factory;
-        $this->entityTypeManager = $entity_type_manager;
-    }
+  /**
+   * Constructs a AdClickCommandController object.
+   *
+   * @param \Drupal\Core\KeyValueStore\KeyValueExpirableFactoryInterface $key_value_expire_factory
+   *   The key value expirable factory.
+   */
+  public function __construct(KeyValueExpirableFactoryInterface $key_value_expire_factory, EntityTypeManagerInterface $entity_type_manager) {
+    $this->keyValueExpireFactory = $key_value_expire_factory;
+    $this->entityTypeManager = $entity_type_manager;
+  }
 
-    /**
-     * {@inheritdoc}
-     */
-    public static function create(ContainerInterface $container) {
-        return new static(
-            $container->get('keyvalue.expirable'),
-            $container->get('entity_type.manager')
-        );
-    }
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+        $container->get('keyvalue.expirable'),
+        $container->get('entity_type.manager')
+    );
+  }
 
-    public static function commandList() {
-        return self::entityQuery('click_command_clicks')->condition('ccid', $id)->execute()->fetchAll();
-    }
+  /**
+   *
+   */
+  public static function commandList() {
+    return self::entityQuery('click_command_clicks')->condition('ccid', $id)->execute()->fetchAll();
+  }
 
 }
--- src/Form/AdClickCommandDeleteForm.php
+++ src/Form/AdClickCommandDeleteForm.php
@@ -2,7 +2,6 @@
 
 namespace Drupal\adclickcommand\Form;
 
-use Drupal\Core\Entity\AdClickCommandConfirmFormBase;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Url;
 
@@ -13,44 +12,44 @@ use Drupal\Core\Url;
  */
 class AdClickCommandDeleteForm extends ContentEntityConfirmFormBase {
 
-    /**
-     * {@inheritdoc}
-     */
-    public function getQuestion() {
-        return $this->t('Are you sure you want to delete entity %name?', array('%name' => $this->entity->label()));
-    }
+  /**
+   * {@inheritdoc}
+   */
+  public function getQuestion() {
+    return $this->t('Are you sure you want to delete entity %name?', array('%name' => $this->entity->label()));
+  }
 
-    /**
-     * {@inheritdoc}
-     *
-     * If the delete command is canceled, return to the contact list.
-     */
-    public function getCancelUrl() {
-        return new Url('entity.adclickcommand.collection');
-    }
+  /**
+   * {@inheritdoc}
+   *
+   * If the delete command is canceled, return to the contact list.
+   */
+  public function getCancelUrl() {
+    return new Url('entity.adclickcommand.collection');
+  }
 
-    /**
-     * {@inheritdoc}
-     */
-    public function getConfirmText() {
-        return $this->t('Delete');
-    }
+  /**
+   * {@inheritdoc}
+   */
+  public function getConfirmText() {
+    return $this->t('Delete');
+  }
 
-    /**
-     * {@inheritdoc}
-     *
-     * Delete the entity and log the event. logger() replaces the watchdog.
-     */
-    public function submitForm(array &$form, FormStateInterface $form_state) {
-        $entity = $this->getEntity();
-        $entity->delete();
+  /**
+   * {@inheritdoc}
+   *
+   * Delete the entity and log the event. logger() replaces the watchdog.
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    $entity = $this->getEntity();
+    $entity->delete();
 
-        $this->logger('adclickcommand')->notice('@type: deleted %title.',
-            array(
-                '@type' => $this->entity->bundle(),
-                '%title' => $this->entity->label(),
-            ));
-        $form_state->setRedirect('entity.adclickcommand.collection');
-    }
+    $this->logger('adclickcommand')->notice('@type: deleted %title.',
+        array(
+          '@type' => $this->entity->bundle(),
+          '%title' => $this->entity->label(),
+        ));
+    $form_state->setRedirect('entity.adclickcommand.collection');
+  }
 
 }
